283cb55c89a595763f52b258f5248faa8273ffeb
Ahmad Doleh <ahmad@doleh.com>
2021-02-04 10:54:49 -0500

    Added item and vending machine class. Pushing to Todd

---
+++ 19_Capstone/Capstone/Models/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    class Item
+    {
+        public string Name { get; }
+        public string Type { get; }
+        public string SlotLocation { get; }
+        public int Quantity { get; set; }
+        public decimal Price { get; }
+        public string SoundEffect
+        {
+            get
+            {
+                return (Type.ToLower() == "chip") ? "Crunch Crunch, Yum!" :
+                    (Type.ToLower() == "drink") ? "Glug Glug, Yum!" :
+                    (Type.ToLower() == "gum") ? "Chew Chew, Yum!" :
+                    "Munch Munch, Yum!" ;
+            }
+        }
+
+        public Item(string name, string type, string slotlocation, decimal price)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.SlotLocation = slotlocation;
+            this.Price = price;
+            Quantity = 5;
+        }
+    }
+}
---
+++ 19_Capstone/Capstone/Models/VendingMachine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    class VendingMachine
+    {
+        private List<Item> inventory = new List<Item> { };
+        public Item[] Inventory { get { return inventory.ToArray(); } }
+        
+        private decimal balance = 0;
+        public decimal Balance { get { return balance; } }
+
+        public void Dispense()
+        {
+
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public bool Accounting(decimal money)
+        {
+            if (balance + money < 0)
+            {
+                balance += money;
+                return true;
+            }
+
+            return false;
+        }
+
+        public Dictionary<decimal, int> Change()
+        {
+            Dictionary<decimal, int> change = new Dictionary<decimal, int> { { .25M, 0 }, { .1M, 0 }, { .05M, 0 }, { .01M, 0 } };
+
+            //TODO calculate amount of each coin needed from balance
+
+            return change;
+        }
+    }
+}
